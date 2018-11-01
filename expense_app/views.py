67de4b1ee49071a7102202a8e391b82273df614b
Aditya Karnik <aditya.s.karnik@gmail.com>
2018-11-01 17:23:51 +0530

    json data loaded successfuly

--- expense_app/views.py
+++ expense_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 import pytz
-from django.template.loader import get_template
+from django.template.loader import get_template, render_to_string
 from datetime import datetime, timezone
 import dateutil.parser
 from .apps import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+import json
 # Create your views here.
 
 
@@ -19,10 +19,16 @@ def index(request):
     now = dateutil.parser.parse(new_now)
     # html = "<html><body>It is now %s</body></html>" % now
     t = get_template('dashboard.html')
-    html = t.render({'current_date': now})
-    return HttpResponse(html)
+    # html = t.render({'current_date': now})
+    with open('/home/adityakarnik/console_projects/expense_manager/expense_data.json') as d:
+        data = json.loads(d.read())
+    
+    return render(request, 'dashboard.html', {'data':data})
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+with open('/home/adityakarnik/console_projects/expense_manager/expense_data.json') as d:
+    data = json.load(d)
